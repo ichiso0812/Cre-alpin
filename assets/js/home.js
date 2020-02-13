c9f3b490a7267968a1448489b8941affd20eccc8
Jean Takanacare <luka.ouddanne@gmail.com>
2020-02-13 16:54:28 +0100

    index

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,4 +1,9 @@
 $(document).ready(function () {
+    //menu
+    $("#burger_menu").click(function(e){
+        $("#menu").toggleClass("active")
+    })
+    //slider
     $(".slider").slick({
         infinite: true,
         slidesToShow: 2,
